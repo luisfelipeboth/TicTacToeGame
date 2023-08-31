667dbeaa1d04942ab46e412c1dc5aa219f833566
Luis Felipe Both <44323782+luisfelipeboth@users.noreply.github.com>
2023-08-31 18:51:24 -0400

    Finishing Game.py
    Game finished!

--- Game.py
+++ Game.py
@@ -1,17 +1,5 @@
-'''
-    Features in my game:
-    -- make a counter of winnings of each player (can use dictionary here)
-    -- run a sort to see wich player will be playing first (can use random numbers and check if it is even or not)
-    -- assign X to player one and O to player two (always that, show this information to the player (the player Luis will be playing as X!, only for the first player))
-
-    Logic in my game:
-    -- Initialize variables and display
-    -- Ask for input
-    -- check input
-    -- Update the board
-    -- check keep playing
-'''
 from os import system,name
+import random
 
 def clear_screen():
     if name == 'nt':
@@ -25,13 +13,17 @@ def ask_names():
     print("\n")
     return player1, player2
 
-def display(row1,row2,row3,dict):
+def display(row1,row2,row3,dict,data):
     keys = list(dict.keys())
     values = list(dict.values())
+    names = list(data.keys())
+    markers = list(data.values())
     clear_screen()
     print("The score of the game is:")
     print(f"{keys[0]}: {values[0]}")
     print(f"{keys[1]}: {values[1]}")
+    print(f"The player {names[0]} is the marker: {markers[0]}")
+    print(f"The player {names[1]} is the marker: {markers[1]}")
     print("---------------------------------------------")
     print("Use your keyboard design to select the place!")
     print("---------------------------------------------\n")
@@ -68,7 +60,7 @@ def check_column_index(position):
         column_index = 0
     return column_index
 
-def replacement_choice(game_matrix, position, data):
+def replacement_choice(game_matrix, intern_matrix, position, data):
     if position in range(7,10):
         row_index = 0
     elif position in range(4,7):
@@ -77,10 +69,11 @@ def replacement_choice(game_matrix, position, data):
         row_index = 2
 
     column_index = check_column_index(position)
+    intern_matrix[row_index][column_index] = data
     if position == 7 or position == 4 or position == 1:
         data = "  " + data
     game_matrix[row_index][column_index] = data
-    return game_matrix
+    return game_matrix, intern_matrix
 
 def keep_playing():
     choice = "WRONG"
@@ -94,18 +87,74 @@ def keep_playing():
     else:
         return False
 
-def check_winner():
-    pass
+def check_winner(game_matrix, marker, dict):
+    winner_name = ""
+    winner = False
 
-def update_score():
-    pass
+    # Check if there is a winner
+    if game_matrix[0][0] == game_matrix[0][1] == game_matrix[0][2] == "X" or game_matrix[0][0] == game_matrix[0][1] == game_matrix[0][2] == "O":
+        winner = True
+    elif game_matrix[1][0] == game_matrix[1][1] == game_matrix[1][2] == "X" or game_matrix[1][0] == game_matrix[1][1] == game_matrix[1][2] == "O":
+        winner = True
+    elif game_matrix[2][0] == game_matrix[2][1] == game_matrix[2][2] == "X" or game_matrix[2][0] == game_matrix[2][1] == game_matrix[2][2] == "O":
+        winner = True
+    elif game_matrix[0][0] == game_matrix[1][0] == game_matrix[2][0] == "X" or game_matrix[0][0] == game_matrix[1][0] == game_matrix[2][0] == "O":
+        winner = True
+    elif game_matrix[0][1] == game_matrix[1][1] == game_matrix[2][1] == "X" or game_matrix[0][1] == game_matrix[1][1] == game_matrix[2][1] == "O":
+        winner = True
+    elif game_matrix[0][2] == game_matrix[1][2] == game_matrix[2][2] == "X" or game_matrix[0][2] == game_matrix[1][2] == game_matrix[2][2] == "O":
+        winner = True
+    elif game_matrix[0][0] == game_matrix[1][1] == game_matrix[2][2] == "X" or game_matrix[0][0] == game_matrix[1][1] == game_matrix[2][2] == "O":
+        winner = True
+    elif game_matrix[2][0] == game_matrix[1][1] == game_matrix[0][2] == "X" or game_matrix[2][0] == game_matrix[1][1] == game_matrix[0][2] == "O":
+        winner = True
+    else:
+        winner = False
+    
+    # Check winner name
+    winner_name = list(dict.keys())[list(dict.values()).index(marker)]
+    print(game_matrix)
+    return winner, winner_name
 
-def winner_screen():
-    pass
+def update_score(player, dict):
+    if player == 1:
+        dict[player] = dict[player] + 1
+    else:
+        dict[player] = dict[player] + 1
+
+    return dict
+
+def winner_screen(dict,winner_name):
+    clear_screen()
+    keys = list(dict.keys())
+    values = list(dict.values())
+    print("---------------------------------------")
+    print(f"The player {winner_name} was the winner!")
+    print("---------------------------------------\n")
+    print("---------------------------------------")
+    print("The final score is:")
+    print(f"{keys[0]}: {values[0]}")
+    print(f"{keys[1]}: {values[1]}")
+    print("---------------------------------------\n")
+    return keep_playing()
 
 def not_winner_screen():
     # there is no winner, ask for keep playing
-    pass
+    clear_screen()
+    print("----------------------")
+    print("There wasn't a winner!")
+    print("----------------------\n")
+    return keep_playing()
+
+def randomize_player(playerOneName,playerTwoName):
+    rand_number = random.randint(1,100)
+    if rand_number%2 == 0:
+        first_player = playerOneName
+        second_player = playerTwoName
+    else:
+        first_player = playerTwoName
+        second_player = playerOneName
+    return first_player,second_player
 
 def main_game():
     # Initialize variables
@@ -115,10 +164,12 @@ def main_game():
     player_playing = 1
     score = {playerOneName:0,playerTwoName:0}
     game_matrix = [["   "," "," "],["   "," "," "],["   "," "," "]]
+    intern_matrix = [["   "," "," "],["   "," "," "],["   "," "," "]]
     rows = ["  |  ".join(game_matrix[0]),"  |  ".join(game_matrix[1]),"  |  ".join(game_matrix[2])]
     valid_inputs = [1,2,3,4,5,6,7,8,9]
     keep_playing_flag = True
     game_on = True
+    winner = False
     
     # Starter tasks
     playerOneName, playerTwoName = ask_names()
@@ -127,33 +178,59 @@ def main_game():
     del score["1"]
     del score["2"]
 
+    first, second = randomize_player(playerOneName,playerTwoName)
+    data = {first:"X",second:"O"}
+
     # Repeat tasks
     while keep_playing_flag:
         while game_on:
-            display(rows[0],rows[1],rows[2],score)
+            display(rows[0],rows[1],rows[2],score,data)
             choice = player_input(valid_inputs)
+            #check player plaing
             if player_playing == 1:
-                data = "X"
+                marker = data[first]
             else:
-                data = "O"
-            game_matrix = replacement_choice(game_matrix,choice,data)
+                marker = data[second]
+            game_matrix, intern_matrix = replacement_choice(game_matrix,intern_matrix,choice,marker)
             rows = ["  |  ".join(game_matrix[0]),"  |  ".join(game_matrix[1]),"  |  ".join(game_matrix[2])]
-            display(rows[0],rows[1],rows[2],score)
+            display(rows[0],rows[1],rows[2],score,data)
             valid_inputs.remove(choice)
-            if len(valid_inputs) == 0:
+            winner, winner_name = check_winner(intern_matrix, marker, data)
+            
+            # Change player playing
+            player_playing = (player_playing + 1) % 2   
+            if winner:
                 game_on = False
+                score = update_score(winner_name, score)
+                keep_playing_flag = winner_screen(score, winner_name)
+                first, second = randomize_player(playerOneName,playerTwoName)
+                if keep_playing_flag:
+                    # Re-Initialize the matrix and some variables
+                    game_on = True
+                    first, second = randomize_player(playerOneName,playerTwoName)
+                    data = {first:"X",second:"O"}
+                    player_playing = 1
+                    valid_inputs = [1,2,3,4,5,6,7,8,9]
+                    game_matrix = [["   "," "," "],["   "," "," "],["   "," "," "]]
+                    intern_matrix = [["   "," "," "],["   "," "," "],["   "," "," "]]
+                    rows = ["  |  ".join(game_matrix[0]),"  |  ".join(game_matrix[1]),"  |  ".join(game_matrix[2])]
 
-            # check winner
-            # update score
-            # if winner, display winner screen
-            player_playing = (player_playing + 1) % 2
+            if len(valid_inputs) == 0 and not winner:
+                game_on = False
+                keep_playing_flag = not_winner_screen()
+                first, second = randomize_player(playerOneName,playerTwoName)
+                if keep_playing_flag:
+                    # Re-Initialize the matrix and some variables
+                    game_on = True
+                    first, second = randomize_player(playerOneName,playerTwoName)
+                    data = {first:"X",second:"O"}
+                    player_playing = 1
+                    valid_inputs = [1,2,3,4,5,6,7,8,9]
+                    game_matrix = [["   "," "," "],["   "," "," "],["   "," "," "]]
+                    intern_matrix = [["   "," "," "],["   "," "," "],["   "," "," "]]
+                    rows = ["  |  ".join(game_matrix[0]),"  |  ".join(game_matrix[1]),"  |  ".join(game_matrix[2])]
 
-        # Re-Initialize the matrix and some variables
-        print("Game finished!")
-        game_on = True
-        valid_inputs = [1,2,3,4,5,6,7,8,9]
-        game_matrix = [["   "," "," "],["   "," "," "],["   "," "," "]]
-        rows = ["  |  ".join(game_matrix[0]),"  |  ".join(game_matrix[1]),"  |  ".join(game_matrix[2])]
-        keep_playing_flag = keep_playing() # put inside not_winner_screen function
 
+
+### RUN THE HOLE PROGRAM
 main_game()
